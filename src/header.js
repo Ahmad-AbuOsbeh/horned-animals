0c286b921949e2253e25205fe4217322b1a42a7e
Ahmad Abu Osbeh <ahmadabuosbeh20@gmail.com>
2021-05-16 23:10:35 +0300

    finished lab01, first react app

---
+++ src/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.Component{
+    render(){
+        return(
+            <header>
+                <h1>information of horned animals</h1>
+            </header>
+        )
+    }
+}
+
+export default Header;
